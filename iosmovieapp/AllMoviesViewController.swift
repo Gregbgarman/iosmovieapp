2390c82a7f335987f4d76aa77e3966c4f3a8c2cb
Gregbgarman <gregory.b.garman@gmail.com>
2021-12-16 16:52:34 -0500

    Update AllMoviesViewController.swift

--- iosmovieapp/AllMoviesViewController.swift
+++ iosmovieapp/AllMoviesViewController.swift
@@ -53,7 +53,7 @@ class AllMoviesViewController: UIViewController, UICollectionViewDataSource, UIC
     
     
     func QueryMovies(PageNumber:String) -> Void {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page=" + PageNumber)!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=&page=" + PageNumber)!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
